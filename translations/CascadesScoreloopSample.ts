6c272b20ce14a97f64100d4b53a1d791ba3c244b
James Paul Muir <james.p.muir@gmail.com>
2012-11-13 20:51:08 -0500

    initial commit

---
+++ translations/CascadesScoreloopSample.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Game</name>
+    <message>
+        <location filename="../assets/Game.qml" line="14"/>
+        <source>GAME OVER!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/Game.qml" line="30"/>
+        <source>Submit Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/Game.qml" line="41"/>
+        <source>Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="113"/>
+        <source>Welcome</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="34"/>
+        <source>Cascades Scoreloop Sample</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
